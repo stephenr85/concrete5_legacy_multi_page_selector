2e3346d68f163f6ab848dc16c16d49ff205ba419
Stephen <Stephen@.in.eSiteful.com>
2011-08-30 17:51:00 -0400

    Updated multi_page_selector block view to use the navigation helper for generating links.

--- blocks/multi_page_selector/view.php
+++ blocks/multi_page_selector/view.php
@@ -1,11 +1,11 @@
 <?php
-
+	$nh = Loader::helper('navigation');
 	echo "<ul>";
 	foreach($cIDArray as $cID){
 		$page = Page::getByID($cID);
 		if(is_object($page)){
 			$name = $page->getCollectionName();
-			$path = $page->getCollectionPath();
+			$path = $nh->getLinkToCollection($page);
 			echo "<li><a href=\"$path\">$name</a></li>";	
 		}
 	}
